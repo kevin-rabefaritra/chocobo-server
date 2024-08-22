73c6bc9afd6d7c031587d73df264a7d822b6d716
Kevin Michel <kevin.rabefaritra@gmail.com>
2024-08-22 17:01:20 +0200

    Created tests for CORS access Deleted WebConfig and created cors config in SecurityConfig file

--- src/main/java/studio/startapps/chocobo/ChocoboConfiguration.java
+++ src/main/java/studio/startapps/chocobo/ChocoboConfiguration.java
@@ -3,9 +3,8 @@ package studio.startapps.chocobo;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import studio.startapps.chocobo.security.SecurityConfig;
-import studio.startapps.chocobo.security.WebConfig;
 
 @Configuration
-@Import({SecurityConfig.class, WebConfig.class})
+@Import({SecurityConfig.class})
 public class ChocoboConfiguration {
 }
